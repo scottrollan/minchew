db4914eb22c0fc80cfea6e77513a90a69bfc3fd4
Barry Rollan <barryrollan@macbook-pro.lan>
2020-08-06 14:15:59 -0400

    finished About again

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import photo from '../assets/negotiate.jpg';
 import styles from './About.module.scss';
 
 const About = () => {
   return (
-    <div className={styles.About}>
-      <h2>About</h2>
+    <div className={styles.about}>
+      <p>
+        Specializing in all forms of electronic media, Michele Minchew brings
+        twenty six years of media buying and selling experience to her partner
+        clients.
+      </p>
+      <p>
+        At the helm of Minchew Media, Michele has sold almost every media she
+        buys - including spot radio &amp; cable, network cable and digital. What
+        that means to you is that you get a media services partner with
+        experience on both sides of the negotiating table.
+      </p>
+      <img src={photo} alt="" />
+      <p>
+        Michele understands how media is negotiated and placed, and leverages
+        strong vendor partnerships around the country.
+      </p>
+      <p>
+        Having experience on both sides of the negotiating table allows her to
+        see twice as well when planning your project.
+      </p>
     </div>
   );
 };
