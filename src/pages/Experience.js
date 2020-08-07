e81a6f590d79f56432b79f1e8c4f3ea2f0e8c52e
Barry Rollan <barryrollan@MacBook-Pro.local>
2020-08-07 11:28:25 -0400

    added experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import photo from '../assets/gears.jpg';
 import styles from './Experience.module.scss';
 
 const Experience = () => {
   return (
-    <div className={styles.Experience}>
-      <h2>Experience</h2>
+    <div className={styles.experience}>
+      <div className={styles.overlay}></div>
+      <img src={photo} alt="" />
+      <div className={styles.sticky}>
+        <div className={styles.pageName}>
+          <h1 className={styles.content}>Experience</h1>
+        </div>
+      </div>
+      <p>
+        Satisfied clients are at the heart of what we do. We're pleased to count
+        the following among some of the brands we have worked for:{' '}
+      </p>
+      <div className={styles.clients}>
+        <span>Amtrak</span>
+        <span>e.p.t.</span>
+        <span>Kitchen Basics</span>
+        <span>Monistat</span>
+        <span>Nature Sweet Tomatoes</span>
+        <span>Nix</span>
+        <span>Penguin Books</span>
+        <span>Professional Products Unlimited</span>
+        <span>Pure Romance</span>
+        <span>Sucrets</span>
+        <span>Visionworks Of America</span>
+      </div>
     </div>
   );
 };
